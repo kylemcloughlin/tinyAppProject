73c43bf622321ca033132b1d6774362f9a2e120e
kyle mcloughlin <kylemcloughlin1000@hotmail.ca>
2019-02-13 00:34:02 +0000

    partial header applied appwide

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs');
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -10,12 +11,19 @@ var urlDatabase = {
 app.get("/", (req, res) => {
   res.send("eyyyyy!");
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase}
+  res.render('urls_index', templateVars)
 });
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+  
+  app.get("/urls/:shortURL", (req, res) => {
+    const shortURL = req.params.shortURL;
+    const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+    res.render("urls_show", templateVars);
   });
-app.listen(PORT, () => {
+
+  app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
